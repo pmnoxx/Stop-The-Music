d4a3c1bca2bf3c976f3c82ba210c88e2565aaa0a
Piotr Mikulski <piotr@imo.im>
2019-04-23 16:25:19 -0700

    disable tracks

--- playScript.js
+++ playScript.js
@@ -1,6 +1,7 @@
 //Play the video
 console.log('play');
-var actualCode = 'document.getElementsByTagName("video")[0].play();';
+var actualCode = 'document.getElementsByTagName("video")[0].play(); document.getElementsByTagName("video")[0].textTracks[0].mode = "disabled" ';
+
 
 var script = document.createElement('script');
 script.textContent = actualCode;
