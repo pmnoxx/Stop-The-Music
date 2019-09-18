5ea5bab9778dc319ecf39996a0f389d9fe56dbae
p <p>
2019-09-18 07:43:12 -0700

    change default playback speed

--- pauseScript.js
+++ pauseScript.js
@@ -1,6 +1,6 @@
 //Pause the video
 console.log("pause")
-var actualCode = 'document.getElementsByTagName("video")[0].pause();document.getElementsByTagName("video")[0].style.zIndex = 333;document.getElementsByTagName("video")[0].controls = true;';
+var actualCode = 'document.getElementsByTagName("video")[0].pause();document.getElementsByTagName("video")[0].style.zIndex = 333;;document.getElementsByTagName("video")[0].controls = true;document.getElementsByTagName("video")[0].playbackRate = 1.4;';
 
 var script = document.createElement('script');
 script.textContent = actualCode;
--- playScript.js
+++ playScript.js
@@ -1,6 +1,6 @@
 //Play the video
 console.log('play');
-var actualCode = 'document.getElementsByTagName("video")[0].play();document.getElementsByTagName("video")[0].style.zIndex = 333;document.getElementsByTagName("video")[0].controls = true;';
+var actualCode = 'document.getElementsByTagName("video")[0].play();document.getElementsByTagName("video")[0].style.zIndex = 333;document.getElementsByTagName("video")[0].controls = true;document.getElementsByTagName("video")[0].playbackRate = 1.4;';
 
 
 var script = document.createElement('script');
